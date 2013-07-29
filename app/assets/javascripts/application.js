6c2aedcc8952b5b2ceb2398c87db6a225a119037
Max Jacobson <maxwell.jacobson@gmail.com>
2013-07-29 14:43:24 -0400

    gets tooltips kinda working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,28 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require_tree ./../../../vendor/assets/javascripts
 //= require_tree .
+
+$.fn.highlightLine = function() {
+
+    $(this).on("mouseenter", function() {
+      $(this).css("color", "green");
+    });
+
+    $(this).on("mouseleave", function() {
+      $(this).css("color", "black");
+    });
+
+}
 
 $(function() {
   var faye = new Faye.Client('http://localhost:9292/faye');
   faye.subscribe('/receive', function (data) {
-    $("<span class='line'>" + data + "</span>").appendTo(".story-inner");
+    $("<span class='line' data-toggle='tooltip' title='Written by "+ data.author +"' >" + data.content + "</span>").appendTo(".story-inner");
+    $('.line').tooltip();
+    $('.line').highlightLine();
   });
+  $('.line').tooltip();
+  $('.line').highlightLine();
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,8 @@ module ApplicationHelper
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
-  def message_push(message)
-    Net::HTTP.post_form(URI.parse("http://localhost:9292/faye"), :message => {:channel => "/receive", :data => message}.to_json)
+  def message_push(content, author)
+    Net::HTTP.post_form(URI.parse("http://localhost:9292/faye"), :message => {:channel => "/receive", :data => {:content => content, :author => author}}.to_json)
   end
   
 end
