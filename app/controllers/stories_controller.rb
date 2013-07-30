7db92aa734e569a9309e6fa72511cec2f1d16cd2
Max Jacobson <maxwell.jacobson@gmail.com>
2013-07-30 15:49:14 -0400

    updates style

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,9 +2,13 @@ class StoriesController < ApplicationController
   # GET /stories
   # GET /stories.json
   def index
-    @stories = Story.all.reverse
+    # featured
     @story = Story.last
-    # debugger
+
+    # gallery
+    # next 9 stories go in gallery
+    @stories = Story.limit(10).find(:all).reverse.drop(1)
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @stories }
--- db/seeds.rb
+++ db/seeds.rb
@@ -50,5 +50,5 @@ story = Story.new
 story.lines.build(content: Line.random_opening_line, user: plume)
 story.next_user_id = 3
 story.save
-Twilio::SMS.create :to => ernest.phone_number, :from => ENV["TWILIO_NUMBER"],
-                 :body => story.lines.first.content
+# Twilio::SMS.create :to => ernest.phone_number, :from => ENV["TWILIO_NUMBER"],
+#                  :body => story.lines.first.content
