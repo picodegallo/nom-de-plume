cf771f3cfb76ab226a718c75e1512d10160e20d8
Max Jacobson <maxwell.jacobson@gmail.com>
2013-07-31 16:53:30 -0400

    adds better_errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,11 +78,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    @user.destroy if @user == current_user
+    redirect_to logout_path
   end
 end
