5aa2ae9939055a88f117d333b1097bb20ea7a3d6
picodegallo <pocodegallo@gmail.com>
2013-08-01 11:56:25 -0400

    fix js url in write to site avi really never wrote that I promise it is even in the commit log

--- app/models/write_to_site.rb
+++ app/models/write_to_site.rb
@@ -1,6 +1,6 @@
 class WriteToSite
 	def self.push_message(content, author, next_user)
-    uri = URI.parse("http://192.241.168.144:9292/faye.js")
+    uri = URI.parse("http://192.241.168.144:9292/faye")
     data = {:content => content, :author => author, :next_user => next_user}
     message = {:channel => "/receive", :data => data}
     Net::HTTP.post_form(uri, :message => message.to_json)
