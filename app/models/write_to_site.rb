35e41b74807af72f3a3c27e2875d3172f4630128
picodegallo <pocodegallo@gmail.com>
2013-07-31 21:19:45 -0400

    dflaksj

--- app/models/write_to_site.rb
+++ app/models/write_to_site.rb
@@ -1,6 +1,6 @@
 class WriteToSite
 	def self.push_message(content, author, next_user)
-    uri = URI.parse("http://localhost:9292/faye")
+    uri = URI.parse("http://192.241.168.144:9292/faye.js")
     data = {:content => content, :author => author, :next_user => next_user}
     message = {:channel => "/receive", :data => data}
     Net::HTTP.post_form(uri, :message => message.to_json)
